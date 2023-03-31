b2dd9d3d30e58b6ef01b63bdcceba04f2649a097
bahati <bulozealfani@gmail.com>
2023-03-31 03:32:01 -0500

    added a database

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton,\
-    QMainWindow, QTabWidget
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton,\
+    QMainWindow, QTableWidget, QTableWidgetItem
 import sys
 from PyQt6.QtGui import QAction
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -19,16 +20,27 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about)
         about.setMenuRole(QAction.MenuRole.NoRole)
 
-        table = QTabWidget()
+        self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        # reset the table
+        self.table.setRowCount(0)
+
+        for row_num, row_data in enumerate(result):
+            self.table.insertRow(row_num)
+            for column_num, data in enumerate(row_data):
+                self.table.setItem(row_num, column_num, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
 main_app = MainWindow()
 main_app.show()
+main_app.load_data()
 sys.exit(app.exec())
